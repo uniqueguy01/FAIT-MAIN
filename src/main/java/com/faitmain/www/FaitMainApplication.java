67efa32abceaaeb74ce6636e8422b2e5aaa0e0b9
최현 <chlgus428@naver.com>
2025-04-24 10:44:17 +0900

    오류테스트

---
+++ src/main/java/com/faitmain/www/FaitMainApplication.java
@@ -0,0 +1,13 @@
+package com.faitmain.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaitMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaitMainApplication.class, args);
+	}
+
+}
