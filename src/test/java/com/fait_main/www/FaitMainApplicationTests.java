43820f2e25cb036e1db588bf761a0ef3ae3798d8
chlgus <chlgus428@naver.com>
2025-05-26 09:21:48 +0900

    병합  수정

---
+++ src/test/java/com/fait_main/www/FaitMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fait_main.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaitMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
